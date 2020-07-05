a82dbe895df8228e86dc78cc249150389728ae46
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2020-07-05 19:41:19 +0530

    5 july 2020

---
+++ Capitalize.py
@@ -0,0 +1,35 @@
+'''
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports. For example, alison heck should be capitalised correctly as Alison Heck.
+
+alison heck=>Alison Heck
+Given a full name, your task is to capitalize the name appropriately.
+
+Input Format:
+A single line of input containing the full name, S.
+
+Output Format:
+Print the capitalized string, S.
+
+Sample Input:
+chris alan
+
+Sample Output:
+Chris Alan
+'''
+def solve(s):
+    for i in s.split():
+        s = s.replace(i,i.capitalize())
+    return s
+        
+        
+   
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
