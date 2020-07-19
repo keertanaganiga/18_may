79dc4e1669681725a705161a743728cb2b9181ef
keertanaganiga <43719847+keertanaganiga@users.noreply.github.com>
2020-07-19 20:53:31 +0530

    19 july 2020

---
+++ reverse.py
@@ -0,0 +1,24 @@
+'''
+Reverse words in a given String in Python We are given a string and we need to reverse words of given string ?
+Examples:
+Input : 
+str = "AIET CHALLENGES IIT"
+
+Output :
+str = "IIT CHALLENGES AIET"
+
+'''
+
+
+
+str1="AIET CHALLENGES IIT" 
+
+print(str1[::-1])
+
+
+'''
+another solution:
+str1=input()
+
+print(str1[::-1])
+'''
